5d17046d97ddd54ba0f85b8792b37d30661cd06d
Артём <Temmik007@myarch.localdomain>
2025-09-13 18:02:37 +0300

    Adding a house project

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("     /\\\n");
+    printf("    /  \\\n");
+    printf("   /____\\\n");
+    printf("  |  _   |\n");
+    printf("  | |_|  |\n");
+    printf("  |______|");
+    return 0;
+}
